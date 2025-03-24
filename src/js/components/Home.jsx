812c8b9dbc810e234e6a0cd12ddcf9fd9b2d1204
Federico ferreyra <128550575+fedepaino@users.noreply.github.com>
2025-03-24 15:10:32 +0000

    se creo funciones para escribir en input y agregar lista

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,45 @@
 import React from "react";
+import { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [inputValue, setInputValue] = useState("")
+	const [tareas, setTareas] = useState([])
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const cambiarTarea = (e)=> {
+		setInputValue(e.target.value)
+	}
+
+	const agregarTarea = () => {
+		setTareas([...tareas, inputValue])
+		setInputValue("") //esto esoara limpiar el input
+
+	
+
+
+
+	}
+
+	return (
+		<div className="d-flex justify-content-center mt-5" style={{height: "400px"}}>
+			<div className="bg-body-secondary text-center" style={{ width: "300px" }}>
+				<h1 className="text-center mt-5">Hello Rigo!</h1>
+				<input type="text" value={inputValue} onChange={cambiarTarea}/>
+				<button className="btn-add" onClick={agregarTarea}>Agregar</button>
+				<ul>
+					{
+						tareas.length > 0 ? tareas.map(
+							(tarea, index) => {
+								return(
+									<li key={index} className="border-secondary border-bottom m-1" onClick={null}>{tarea}<button className="m-1">X</button></li>
+								)
+							})
+							: <p>No hay tarea</p>
+					}
+				</ul>
+			</div>
 		</div>
 	);
 };
