8772b68d10c87080961a7925ebb7162a8307026d
Federico ferreyra <128550575+fedepaino@users.noreply.github.com>
2025-03-25 10:28:02 +0000

    se creo funcion para eliminar tareas de las lista

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -7,8 +7,11 @@ import { useState } from "react";
 const Home = () => {
 	const [inputValue, setInputValue] = useState("")
 	const [tareas, setTareas] = useState([])
+	const [hoverIndex, setHoverIndex] = useState(null);
 
-	const cambiarTarea = (e)=> {
+
+
+	const cambiarTarea = (e) => {
 		setInputValue(e.target.value)
 	}
 
@@ -16,27 +19,46 @@ const Home = () => {
 		setTareas([...tareas, inputValue])
 		setInputValue("") //esto esoara limpiar el input
 
-	
-
-
-
+	}
+	const deleteTarea = (index) => {
+		const tareaCreada = tareas.filter((tareas, i)=> i !== index)
+		setTareas(tareaCreada)
 	}
 
+
+	
+
 	return (
-		<div className="d-flex justify-content-center mt-5" style={{height: "400px"}}>
-			<div className="bg-body-secondary text-center" style={{ width: "300px" }}>
-				<h1 className="text-center mt-5">Hello Rigo!</h1>
-				<input type="text" value={inputValue} onChange={cambiarTarea}/>
-				<button className="btn-add" onClick={agregarTarea}>Agregar</button>
-				<ul>
+		<div className="d-flex justify-content-center mt-5" style={{ height: "400px" }}>
+			<div className="card bg-body-secondary">
+				<h1 className="text-center mt-5">Mis tareas</h1>
+				<div className="d-flex m-3">
+					<input className="form-control" type="text" value={inputValue} onChange={cambiarTarea} />
+					<button className="btn btn-success" onClick={agregarTarea}>Agregar</button>
+				</div>
+
+				<ul className="list-group list-group-flush">
 					{
 						tareas.length > 0 ? tareas.map(
 							(tarea, index) => {
-								return(
-									<li key={index} className="border-secondary border-bottom m-1" onClick={null}>{tarea}<button className="m-1">X</button></li>
+								return (
+									<li 
+									key={index} 
+									className="list-group-item mx-3 m-1 d-flex justify-content-between" 
+									onMouseEnter={() => setHoverIndex(index)}
+                                	onMouseLeave={() => setHoverIndex(null)}
+									>
+										{tarea}{hoverIndex === index && (
+										<button 
+										className=" bg-danger" 
+										onClick={()=> {deleteTarea(index)}}
+										>
+											<i className="fa-solid fa-trash-can"></i>
+										</button>)}
+									</li>
 								)
 							})
-							: <p>No hay tarea</p>
+							: <p className="list-group-item mx-3 m-1">No hay tareas</p>
 					}
 				</ul>
 			</div>
